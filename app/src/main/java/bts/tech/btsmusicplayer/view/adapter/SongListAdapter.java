5fce7218fd03e83f914baa564e7664fdbe073ec6
tuengominh <tue.ngominh@gmail.com>
2019-06-09 05:18:59 +0200

    Add SongProvider

--- app/src/main/java/bts/tech/btsmusicplayer/view/adapter/SongListAdapter.java
+++ app/src/main/java/bts/tech/btsmusicplayer/view/adapter/SongListAdapter.java
@@ -4,77 +4,46 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.annotation.Nullable;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import bts.tech.btsmusicplayer.R;
 import bts.tech.btsmusicplayer.model.Song;
 
-public class SongListAdapter extends RecyclerView.Adapter<SongListAdapter.MediaPlayerHolder> {
+public class SongListAdapter extends ArrayAdapter<Song> {
 
     /** To inflate the song list into a list view */
 
-    private final Context context;
-    private List<Song> songList = new ArrayList<Song>();
-    private SongClicked songClicked;
+    private Context context;
+    private int layoutResource;
+    private List<Song> songs;
 
-    public interface SongClicked {
-        void onSongClicked(Song song);
-    }
-
-    public SongListAdapter(Context context, List<Song> objects) {
+    public SongListAdapter(@NonNull Context context, int resource, @NonNull List<Song> songs) {
+        super(context, resource, songs);
         this.context = context;
-        this.songList = objects;
-
-    }
-
-    public SongListAdapter(Context context, SongClicked clicked) {
-        this.context = context;
-        songClicked = clicked;
-    }
-
-    class MediaPlayerHolder extends RecyclerView.ViewHolder{
-
-        private ImageView songIcon;
-        private TextView tvTitle;
-        private TextView tvDuration;
-
-        public MediaPlayerHolder(@NonNull View itemView) {
-            super(itemView);
-            this.songIcon = itemView.findViewById(R.id.song_list_adapter_main__icon);
-            this.tvTitle = itemView.findViewById(R.id.song_list_adapter_main__tv__title);
-            this.tvDuration = itemView.findViewById(R.id.song_list_adapter_main__tv__duration);
-        }
+        this.layoutResource = resource;
+        this.songs = songs;
     }
 
     @NonNull
     @Override
-    public MediaPlayerHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.song_list_adapter_main, viewGroup, false);
-        return new MediaPlayerHolder(view);
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = inflater.inflate(layoutResource, null);
+        ImageView img = convertView.findViewById(R.id.song_list_adapter_main__icon);
+        img.setImageResource(context.getResources().getIdentifier(String.valueOf(R.mipmap.ic_launcher_round), "drawable", context.getPackageName()));
+        TextView title = convertView.findViewById(R.id.song_list_adapter_main__tv__title);
+        title.setText(songs.get(position).getTitle());
+        TextView body = convertView.findViewById(R.id.song_list_adapter_main__tv__duration);
+        body.setText(songs.get(position).getDuration());
+
+        return convertView;
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull MediaPlayerHolder holder, int position) {
-        holder.tvDuration.setText(songList.get(position).getDuration());
-        holder.tvTitle.setText(songList.get(position).getTitle());
-        holder.songIcon.setImageResource(context.getResources().getIdentifier(songList.get(position).getPath(), "drawable", context.getPackageName()));
-    }
-
-    @Override
-    public int getItemCount() {
-        return songList.size();
-    }
-
-    public void addSongs(ArrayList songs) {
-        songList.clear();
-        songList = songs;
-        notifyDataSetChanged();
-    }
 }
