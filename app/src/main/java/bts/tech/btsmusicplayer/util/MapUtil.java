3891f48e09f83d31e6f90c6e3e3a8342d5120365
tuengominh <tue.ngominh@gmail.com>
2019-06-12 17:33:04 +0200

    Custom Map layout

--- app/src/main/java/bts/tech/btsmusicplayer/util/MapUtil.java
+++ app/src/main/java/bts/tech/btsmusicplayer/util/MapUtil.java
@@ -1,10 +1,39 @@
 package bts.tech.btsmusicplayer.util;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import bts.tech.btsmusicplayer.R;
+
 public class MapUtil {
 
-    /** Util class used to create markers corresponding to Song's countries
-        and return proper information */
+    /** Util class used to get location information of countries
+     * by default it will be Barcelona */
 
-    //TODO: createMaker( String country)
+    public static LatLng getLatLng(String country) {
+        LatLng latLng;
+        switch (country) {
+            case "Brazil" :
+                latLng = new LatLng(-23.533773, -46.625290);
+                break;
+            case "India" :
+                latLng = new LatLng(28.644800, 77.216721);
+                break;
+            case "Indonesia" :
+                latLng = new LatLng(-8.409518, 115.188919);
+                break;
+            case "South Korea" :
+                latLng = new LatLng(37.532600, 127.024612);
+                break;
+            case "USA" :
+                latLng = new LatLng(31.000000, -100.000000);
+                break;
+            case "Iceland" :
+                latLng = new LatLng(64.128288, -21.827774);
+                break;
+            default :
+                latLng = new LatLng(41.390205, 2.154007);
+        }
+        return latLng;
+    }
 
 }
--- app/src/main/java/bts/tech/btsmusicplayer/view/activity/MapActivity.java
+++ app/src/main/java/bts/tech/btsmusicplayer/view/activity/MapActivity.java
@@ -2,7 +2,17 @@ package bts.tech.btsmusicplayer.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -13,13 +23,45 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.List;
+
+import bts.tech.btsmusicplayer.MainPlayerActivity;
 import bts.tech.btsmusicplayer.R;
+import bts.tech.btsmusicplayer.model.Song;
+import bts.tech.btsmusicplayer.service.PlayerService;
+import bts.tech.btsmusicplayer.util.MapUtil;
+import bts.tech.btsmusicplayer.util.SongUtil;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMarkerClickListener, GoogleMap.OnInfoWindowClickListener {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, View.OnClickListener {
 
-    /** Manipulates the Google Maps fragment */
+    /** Manipulates the Google Maps activity */
 
+    //get context
+    protected static final String TAG = MapActivity.class.getSimpleName();
+
+    //fields to handle map & UI items
     private GoogleMap map;
+    private Button btnGoBack;
+    private Button btnPlay;
+    private Marker currentMarker;
+
+    //fields to control the bound service 'PlayerService'
+    private PlayerService playerService;
+    private boolean isBound = false;
+    private List<Song> songs = MainPlayerActivity.getSongs();
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder serviceInfo) {
+            playerService = new PlayerService(serviceInfo);
+            isBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            isBound = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,54 +72,99 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.activity_map__fr__map);
         mapFragment.getMapAsync(this);
+
+        this.btnPlay = findViewById(R.id.activity_map__btn__play);
+        this.btnPlay.setOnClickListener(this);
+
+        this.btnGoBack = findViewById(R.id.activity_map__btn__back);
+        this.btnGoBack.setOnClickListener(this);
     }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers
-     */
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
         map.getUiSettings().setZoomControlsEnabled(true);
 
-        // Add markers to countries
-        //TODO: use MapUtil to add markers to all countries
-        LatLng brazil = new LatLng(41, 2);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(brazil, 14));
-
-        map.setOnMapClickListener(this);
+        // Add markers to countries and move camera the the first country (Brazil)
+        for (Song song : songs) {
+            LatLng latLng = MapUtil.getLatLng(song.getCountry());
+            map.addMarker(new MarkerOptions().position(latLng).title(song.getTitle()));
+        }
+        map.moveCamera(CameraUpdateFactory.newLatLng(MapUtil.getLatLng(songs.get(0).getCountry())));
         map.setOnMarkerClickListener(this);
-        map.setOnInfoWindowClickListener(this);
+
+        final Intent serviceIntent = new Intent(this, PlayerService.class);
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                bindService(serviceIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+            }
+        };
+        thread.start();
+
     }
 
-    //TODO: not allow create Marker on map click
-    @Override
-    public void onMapClick(LatLng latLng) {
-        createMarker(latLng);
-        //TODO: moveCamera(latLng);
-    }
-
-    private void createMarker(LatLng latLng) {
-        map.addMarker(new MarkerOptions().position(latLng).title("Marker for " + latLng.toString())); //TODO: bensoundbrazilsamba
-    }
-
-    //TODO: click to return song detail (with button linked to MainPlayerActivity) below the map fragment
-    // => revise activity_map.xml layout to inlcude song detail layout
     @Override
     public boolean onMarkerClick(Marker marker) {
-        return showToast(marker);
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(marker.getPosition(), 10));
+        currentMarker = marker;
+        Toast.makeText(this, marker.getTitle(), Toast.LENGTH_SHORT).show();
+
+        //TODO: inflate custom image views & text views
+        final ImageView songIcon = findViewById(R.id.activity_map__flag__icon);
+        final TextView songTitle = findViewById(R.id.activity_map__tv__title);
+        final TextView songDuration = findViewById(R.id.activity_map__tv__duration);
+        final TextView songLocation = findViewById(R.id.activity_map__tv__latlgn);
+        final TextView songComment = findViewById(R.id.activity_map__tv__comment);
+
+        for (Song song : songs) {
+            if (song.getTitle().equals(currentMarker.getTitle())) {
+                songIcon.setImageResource(SongUtil.getFlagResId(song.getCountry()));
+                songTitle.setText(song.getTitle());
+                songDuration.setText(song.getDuration());
+                songLocation.setText(MapUtil.getLatLng(song.getCountry()).toString());
+                songComment.setText(song.getComment());
+            }
+        }
+
+        return true;
     }
 
-    private boolean showToast(Marker marker) {
-        Toast.makeText(this, marker.getTitle(), Toast.LENGTH_SHORT).show();
-        return false;
-    }
     @Override
-    public void onInfoWindowClick(Marker marker) {
-        Toast.makeText(this, "Info window clicked", Toast.LENGTH_SHORT).show();
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.activity_map__btn__play:
+                Log.d(TAG,"Prepare to play " + currentMarker.getTitle());
+                for (int i = 0; i < songs.size() - 1; i++) {
+                    if (songs.get(i).getTitle().equals(currentMarker.getTitle())) {
+                        Log.d(TAG, "Playing song with index " + i);
+                        if (isBound) {
+                            Toast.makeText(this, "Now playing " + currentMarker.getTitle(), Toast.LENGTH_SHORT).show();
+                            this.playerService.selectAndPlaySong(i);
+                        }
+                    }
+                }
+                break;
+            case R.id.activity_map__btn__back:
+                Log.d(TAG,"Go to Media Player");
+                Intent intent = new Intent(this, MainPlayerActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                Log.w(TAG, "Not clickable");
+        }
+    }
+
+    //stop PlayerService if isFinishing()
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (isFinishing()) {
+            unbindService(serviceConnection);
+            isBound = false;
+            playerService.stopSelf();
+        }
     }
 }
